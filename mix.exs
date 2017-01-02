bc75890bfb6808d8200255d069de334dd5f11521
Klaus Alfert <klaus.alfert@gmail.com>
2017-01-02 12:24:24 +0100

    Update to Nerves 0.4 release

--- mix.exs
+++ mix.exs
@@ -7,13 +7,13 @@ defmodule LedNerves.Mixfile do
     [app: :led_nerves,
      version: "0.0.1",
      target: @target,
-     archives: [nerves_bootstrap: "~> 0.1.4"],
+     archives: [nerves_bootstrap: "~> 0.2.1"],
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases,
-     deps: deps ++ system(@target)]
+     aliases: aliases(),
+     deps: deps() ++ system(@target)]
   end
 
   # Configuration for the OTP application.
@@ -21,18 +21,24 @@ defmodule LedNerves.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {LedNerves, []},
-     applications: [:logger, :nerves_leds, :nerves]]
+     applications: [:logger,
+      #  :nerves_interim_wifi,
+      :nerves_networking,
+      :nerves_leds, :nerves]]
   end
 
   def deps do
     [
-      {:nerves, "~> 0.3.0"},
-      {:nerves_leds, "~> 0.7.0"}
+      {:nerves, "~> 0.4.0"},
+      {:nerves_leds, "~> 0.7.0"},
+      # {:nerves_interim_wifi, "~> 0.1.0"}
+      {:nerves_networking, "~> 0.6.0"}
     ]
   end
 
   def system(target) do
     [{:"nerves_system_#{target}", ">= 0.0.0"}]
+    # [{:"nerves_system_#{target}", path: "/tmp/nerves_system_#{target}"}]
   end
 
   def aliases do
