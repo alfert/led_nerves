e768d77ddf5ba148fb3e7fd62364c49a6ce3a68e
Klaus Alfert <klaus.alfert@gmail.com>
2017-01-04 10:23:03 +0100

    Start ntp only after we acquired an IP address.

--- lib/led_nerves.ex
+++ lib/led_nerves.ex
@@ -10,12 +10,20 @@ defmodule LedNerves do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    configure_wlan()
-    network_time()
+    start_ntp = fn state ->
+      Logger.info("Network state change: #{inspect state}")
+      if state |> Enum.any?(fn({k,v}) -> k == :status and v == "bound" end) do
+        Logger.info("IP address bound, start ntp ")
+        network_time()
+      end
+    end
+
+    configure_wlan(start_ntp)
+    # network_time()
 
     Blinky.blink()
     Logger.debug "Current time is: #{DateTime.utc_now() |> DateTime.to_string}"
-    
+
     # Define workers and child supervisors to be supervised
     children = [
       # worker(LedNerves.Worker, [arg1, arg2, arg3]),
@@ -25,7 +33,7 @@ defmodule LedNerves do
     Supervisor.start_link(children, opts)
   end
 
-  def configure_wlan() do
+  def configure_wlan(on_change_fun \\ fn(_dict) -> :ok end) do
     Logger.debug "Configure WLAN"
     Logger.debug "modprobe the wifi module"
     System.cmd("/sbin/modprobe", ["8192cu"])
@@ -37,7 +45,7 @@ defmodule LedNerves do
          "-c", "/etc/wpa_supplicant.conf"])
      :timer.sleep(500)
 
-     Networking.setup(@wlan_interface)
+     Networking.setup(@wlan_interface, [on_change: on_change_fun])
 
     # opts = Application.get_env(:led_nerves, @wlan_interface)
     # Logger.debug "WLAN opts: #{inspect opts}"
